61f1209c414019b7b4fc264c995eb71deaec76ca
HeroLeam <thiagoleam@gmail.com>
2023-11-22 16:26:17 -0300

    WIP

--- src/javascript/select.js
+++ src/javascript/select.js
@@ -1,22 +1,29 @@
-const imagemIcone = document.querySelectorAll('.imagemIcone')
-const imagemSelecao = document.querySelectorAll('.imagemSelecao')
+const imagemIcone = document.querySelectorAll(".imagemIcone")
+const imagemSelecao = document.querySelectorAll(".imagemSelecao")
 
-imagemIcone.forEach(function (item) {
-  item.addEventListener("mouseover", function () {
-    const imagemIconeAtivo = document.querySelector(".animarImagem")
-    if (imagemIconeAtivo) {
-      imagemIconeAtivo.classList.remove("animarImagem")
-    }
-    item.classList.add("animarImagem")
-  })
-})
+adicionaAnimacao()
 
-imagemSelecao.forEach(function (item) {
-  item.addEventListener("mouseover", function () {
-    const imagemSelecaoAtivo = document.querySelector(".esconder")
-    if (imagemSelecaoAtivo) {
-      imagemSelecaoAtivo.classList.remove("esconder")
-    }
-    item.classList.add("esconder")
+removeAnimacao()
+
+function removeAnimacao() {
+  imagemIcone.forEach(function (item) {
+    item.addEventListener("mouseout", function () {
+      const imagemIconeAtivo = document.querySelector(".animarImagem")
+      if (imagemIconeAtivo) {
+        imagemIconeAtivo.classList.remove("animarImagem")
+      }
+    })
   })
-})
+}
+
+function adicionaAnimacao() {
+  imagemIcone.forEach(function (item) {
+    item.addEventListener("mouseover", function () {
+      const imagemIconeAtivo = document.querySelector(".animarImagem")
+      if (imagemIconeAtivo) {
+        imagemIconeAtivo.classList.remove("animarImagem")
+      }
+      item.classList.add("animarImagem")
+    })
+  })
+}
