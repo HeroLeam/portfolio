86aff805b66d9365889c134b739cf2de7f202e29
HeroLeam <thiagoleam@gmail.com>
2023-11-24 12:55:20 -0300

    WIP

--- src/javascript/select.js
+++ src/javascript/select.js
@@ -1,30 +1,48 @@
-const imagemIcone = document.querySelectorAll(".imagemIcone")
-const imagemSelecao = document.querySelectorAll(".imagemSelecao")
-const paragrafoTexto = document.querySelectorAll(".texto")
+document.addEventListener("DOMContentLoaded", function () {
 
-adicionaAnimacao()
+  const imagensIcone = document.querySelectorAll(".imagemIcone")
+  const imagensSelecao = document.querySelectorAll(".imagemIconeSelecao")
+  const paragrafoTexto = document.querySelectorAll(".texto")
 
-removeAnimacao()
+  let imagemSelecionadaIndex = null;
 
-function removeAnimacao() {
-  imagemIcone.forEach(function (item) {
-    item.addEventListener("mouseout", function () {
-      const imagemIconeAtivo = document.querySelector(".animarImagem")
-      if (imagemIconeAtivo) {
-        imagemIconeAtivo.classList.remove("animarImagem")
-      }
-    })
-  })
+  imagemSelecionadaIndex = selecaoDosItens(imagensIcone, imagemSelecionadaIndex, imagensSelecao, paragrafoTexto);
+
+  imagemSelecionadaIndex = removeSelecaoDosItens(imagensSelecao, paragrafoTexto, imagemSelecionadaIndex);
+
+})
+
+function removeSelecaoDosItens(imagensSelecao, paragrafoTexto, imagemSelecionadaIndex) {
+  document.addEventListener("mouseout", function () {
+    // Adiciona a classe esconder
+    imagensSelecao.forEach((imagemSelecao) => {
+      imagemSelecao.classList.add("esconder");
+    });
+    paragrafoTexto.forEach((paragrafo) => {
+      paragrafo.classList.add("esconder");
+    });
+    // Resetar o índice da imagem selecionada
+    imagemSelecionadaIndex = null;
+  });
+  return imagemSelecionadaIndex;
 }
 
-function adicionaAnimacao() {
-  imagemIcone.forEach(function (item) {
-    item.addEventListener("mouseover", function () {
-      const imagemIconeAtivo = document.querySelector(".animarImagem")
-      if (imagemIconeAtivo) {
-        imagemIconeAtivo.classList.remove("animarImagem")
+function selecaoDosItens(imagensIcone, imagemSelecionadaIndex, imagensSelecao, paragrafoTexto) {
+  imagensIcone.forEach((imagemIcone, index) => {
+    imagemIcone.addEventListener("mouseover", function () {
+      // Adicionar a classe esconder
+      if (imagemSelecionadaIndex !== null) {
+        imagensSelecao[imagemSelecionadaIndex].classList.add("esconder");
+        paragrafoTexto[imagemSelecionadaIndex].classList.add("esconder");
       }
-      item.classList.add("animarImagem")
-    })
-  })
+
+      // Remover a classe esconder
+      imagensSelecao[index].classList.remove("esconder");
+      paragrafoTexto[index].classList.remove("esconder");
+
+      // Atualizar o índice da imagem selecionada
+      imagemSelecionadaIndex = index;
+    });
+  });
+  return imagemSelecionadaIndex;
 }
