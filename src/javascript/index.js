8469a3a70f753ad769de663d978f091081dddbe2
HeroLeam <thiagoleam@gmail.com>
2023-11-25 17:15:06 -0300

    WIP

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const paragrafoTexto = document.querySelectorAll(".texto")
 
   let imagemSelecionadaIndex = null;
-
+  
   imagemSelecionadaIndex = selecaoDosItens(imagensIcone, imagemSelecionadaIndex, imagensSelecao, paragrafoTexto);
 
   imagemSelecionadaIndex = removeSelecaoDosItens(imagensSelecao, paragrafoTexto, imagemSelecionadaIndex);
+  trocaImagemKeyboard()
+
+  trocaImagemWacom()
 
 })
 
@@ -17,32 +20,56 @@ function selecaoDosItens(imagensIcone, imagemSelecionadaIndex, imagensSelecao, p
     imagemIcone.addEventListener("mouseover", function () {
       // Adicionar a classe esconder
       if (imagemSelecionadaIndex !== null) {
-        imagensSelecao[imagemSelecionadaIndex].classList.add("esconder");
-        paragrafoTexto[imagemSelecionadaIndex].classList.add("esconder");
+        imagensSelecao[imagemSelecionadaIndex].classList.add("esconder")
+        paragrafoTexto[imagemSelecionadaIndex].classList.add("esconder")
       }
 
       // Remover a classe esconder
-      imagensSelecao[index].classList.remove("esconder");
-      paragrafoTexto[index].classList.remove("esconder");
+      imagensSelecao[index].classList.remove("esconder")
+      paragrafoTexto[index].classList.remove("esconder")
 
       // Atualizar o índice da imagem selecionada
-      imagemSelecionadaIndex = index;
-    });
-  });
-  return imagemSelecionadaIndex;
+      imagemSelecionadaIndex = index
+    })
+  })
+  return imagemSelecionadaIndex
 }
 
 function removeSelecaoDosItens(imagensSelecao, paragrafoTexto, imagemSelecionadaIndex) {
   document.addEventListener("mouseout", function () {
     // Adiciona a classe esconder
     imagensSelecao.forEach((imagemSelecao) => {
-      imagemSelecao.classList.add("esconder");
+      imagemSelecao.classList.add("esconder")
     });
     paragrafoTexto.forEach((paragrafo) => {
-      paragrafo.classList.add("esconder");
+      paragrafo.classList.add("esconder")
     });
     // Resetar o índice da imagem selecionada
-    imagemSelecionadaIndex = null;
+    imagemSelecionadaIndex = null
   });
-  return imagemSelecionadaIndex;
+  return imagemSelecionadaIndex
+}
+
+function trocaImagemKeyboard() {
+  document.getElementById('imagemIconeKeyboard').addEventListener('mouseover', function () {
+    // Substitua 'imagem2.jpg' pelo caminho da sua segunda imagem
+    this.src = './src/image/keyboard.gif';
+  });
+
+  document.getElementById('imagemIconeKeyboard').addEventListener('mouseout', function () {
+    // Substitua 'imagem1.jpg' pelo caminho da sua primeira imagem
+    this.src = './src/image/keyboard.png';
+  });
+}
+
+function trocaImagemWacom() {
+  document.getElementById('imagemIconeWacom').addEventListener('mouseover', function () {
+    // Substitua 'imagem2.jpg' pelo caminho da sua segunda imagem
+    this.src = './src/image/wacom.gif';
+  });
+
+  document.getElementById('imagemIconeWacom').addEventListener('mouseout', function () {
+    // Substitua 'imagem1.jpg' pelo caminho da sua primeira imagem
+    this.src = './src/image/wacom.png';
+  });
 }
